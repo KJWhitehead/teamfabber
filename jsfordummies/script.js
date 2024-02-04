315cf42e81948203a5bf1ad7063756b0631b2dfb
KJ Whitehead <kenyon.whitehead@gmail.com>
2024-02-04 12:58:19 +0000

    testing JS for Dummies activities in Ch 3

---
+++ jsfordummies/script.js
@@ -0,0 +1,16 @@
+let interestRate = .06;
+interestRate = interestRate / 12;
+console.log(interestRate);
+
+/*const v1 = true;
+const v2 = 10;
+const v3 = "testing";
+const v4 = false;
+const v5 = 0;
+const v6 = "";
+const leftOperand = 
+    eval(prompt("Enter the left operand (a value or expression:", true));
+const rightOperand = 
+    eval(prompt("Enter the right operand (a value or expression):", true));
+const result = leftOperand && rightOperand;
+console.log(result)*/
